6dc7527814345fdaa36ff5ae62dff8217562a62d
irtezasyed01 <irtezasyed@gmail.com>
2023-06-19 12:51:46 -0400

    updated project

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -7,6 +7,8 @@ except Exception:
     sys.exit()
 
 from django.conf import settings
+from django.contrib import admin
+
 import uuid
 
 
@@ -106,6 +108,15 @@ class Enrollment(models.Model):
     # question text
     # question grade/mark
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='questions')
+    question_text = models.CharField(max_length=1000)
+    grade_point = models.IntegerField()
+
+    def __str__(self):
+        return self.question_text
+
+             
     # <HINT> A sample model method to calculate if learner get the score of the question
     #def is_get_score(self, selected_ids):
     #    all_answers = self.choice_set.filter(is_correct=True).count()
@@ -114,6 +125,16 @@ class Enrollment(models.Model):
     #        return True
     #    else:
     #        return False
+    
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+   
 
 
 #  <HINT> Create a Choice Model with:
@@ -122,8 +143,25 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
+
 # class Choice(models.Model):
 
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+    
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
@@ -132,3 +170,10 @@ class Enrollment(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    chocies = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f"Submission #{self.id} for Enrollment #{self.enrollment.id}"
